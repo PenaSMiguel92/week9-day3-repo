3556bbd9d501b4fbc0157d93e2c0ca60a52652bd
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-19 15:01:05 -0700

    init

---
+++ src/test/java/com/revature/RevatureWeek9Day3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevatureWeek9Day3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
