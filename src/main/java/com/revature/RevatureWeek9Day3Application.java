7dd3350207c15f222c500e5358a87b52bfc0697f
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-20 12:48:42 -0700

    different properties files

--- src/main/java/com/revature/RevatureWeek9Day3Application.java
+++ src/main/java/com/revature/RevatureWeek9Day3Application.java
@@ -3,8 +3,10 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application2.yml")
 public class RevatureWeek9Day3Application {
 
 	public static void main(String[] args) {
